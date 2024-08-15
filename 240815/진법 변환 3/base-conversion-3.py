f648c800346f133c0c965c83499dd1303500153a
장수완 <jsuwan961205@gmail.com>
2024-08-15 20:37:21 +0900

    Title: 진법 변환 3, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240815/진법 변환 3/base-conversion-3.py
+++ 240815/진법 변환 3/base-conversion-3.py
@@ -1,6 +1,8 @@
 hex_num = int(input())
+if hex_num == 0:
+    print(0)
+else:
+    num = int(f"0o{hex_num}", 8)
 
-num = int(f"0o{hex_num}", 8)
-
-bin_num = bin(num)[2:]
-print(bin_num)
+    bin_num = bin(num)[2:]
+    print(bin_num)
