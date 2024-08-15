c22e9b207a1b6d624b4cfa0b9a12db762637b1e6
장수완 <jsuwan961205@gmail.com>
2024-08-15 20:44:17 +0900

    Title: 진법 변환 5, Time: 138ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240815/진법 변환 5/base-conversion-5.py
@@ -0,0 +1,10 @@
+num, formation = map(int, input().split())
+
+if formation == 2:
+    print(bin(num)[2:])
+elif formation == 8:
+    print(oct(num)[2:])
+elif formation == 16:
+    print(hex(num)[:2:])
+else:
+    print('invalid formation')
