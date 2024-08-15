847f980d59c4ff7efd9db216ac893152f767bdf7
장수완 <jsuwan961205@gmail.com>
2024-08-15 20:38:59 +0900

    Title: 진법 변환 3, Time: 112ms, Memory: 27MB, Status: Passed - CodeTree

--- 240815/진법 변환 3/base-conversion-3.py
+++ 240815/진법 변환 3/base-conversion-3.py
@@ -1,8 +1,5 @@
-hex_num = input()
-if hex_num == '0':
-    print(0)
-else:
-    num = int(f"0o{hex_num}", 8)
+hex_input = input()
 
-    bin_num = bin(num)[2:]
-    print(bin_num)
+num = int(f"0o{hex_input}", 8)
+bin_num = bin(num)[2:]
+print(bin_num)
