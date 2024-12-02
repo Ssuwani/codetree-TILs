9f1b7ca606fd8a386bd7b6cd47e99f09d8b59dcc
장수완 <jsuwan961205@gmail.com>
2024-12-02 14:36:21 +0900

    Title: 피보나치 수, Time: 83ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241202/피보나치 수/fibonacci-number.py
+++ 241202/피보나치 수/fibonacci-number.py
@@ -2,14 +2,24 @@
 
 n = int(input())
 
-memo = [-1] * (n+1)
+# memo = [-1] * (n+1)
 
-def fibo(num):
-    if memo[num] != -1:
-        return memo[num]
-    if num == 1 or num == 2:
-        return 1
+# def fibo(num):
+#     if memo[num] != -1:
+#         return memo[num]
+#     if num == 1 or num == 2:
+#         return 1
     
-    return fibo(num-1) + fibo(num-2)
+#     return fibo(num-1) + fibo(num-2)
 
-print(fibo(n))
+# print(fibo(n))
+
+dp = [0] * (n+1)
+
+dp[1] = 1
+dp[2] = 1
+
+for i in range(3, n+1):
+    dp[i] = dp[i-1] + dp[i-2]
+
+print(dp[-1])
