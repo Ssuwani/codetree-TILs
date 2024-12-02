2c8ecd4e5cf543e584f85600ed396f4a9a9ac76e
장수완 <jsuwan961205@gmail.com>
2024-12-02 14:38:39 +0900

    Title: 피보나치 수, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 241202/피보나치 수/fibonacci-number.py
+++ 241202/피보나치 수/fibonacci-number.py
@@ -1,12 +1,12 @@
-
 n = int(input())
 
-dp = [0] * (n+1)
+MAX_NUM = 45
 
-dp[1] = 1
-dp[2] = 1
+dp = [0 for _ in range(MAX_NUM + 1)]
 
-for i in range(3, n+1):
-    dp[i] = dp[i-1] + dp[i-2]
+dp[1] = dp[2] = 1
 
-print(dp[-1])
+for i in range(3, n + 1):
+    dp[i] = dp[i - 1] + dp[i - 2]
+    
+print(dp[n])
